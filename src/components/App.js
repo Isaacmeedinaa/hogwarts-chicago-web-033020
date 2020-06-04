72dcf399180745fd36a75e32565cfa9ace3587d1
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-06-04 12:05:19 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,60 @@
 import React, { Component } from "react";
 import "../App.css";
 import Nav from "./Nav";
+import Filter from './Filter'
 import hogs from "../porkers_data";
-import HelloWorld from "./HelloWorld";
+import HogsBrowser from "./HogsBrowser";
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      hogs: hogs,
+      filters: {
+        toggled: false
+      },
+      sorts: {
+        type: 'name'
+      }
+    }
+  }
+
+  onChangeSortType = (event) => {
+    this.setState({
+      sorts: {
+        type: event.target.value
+      }
+    })
+  }
+
+  onChangeFilter = (event) => {
+    this.setState({
+      filters: {
+        toggled: event.target.checked
+      }
+    })
+  }
+
+  onClickHiddenHogs = (event) => {
+    console.log('hides hogs')
+  }
+
   render() {
     return (
       <div className="App">
         <Nav />
-        <HelloWorld />
+        <Filter 
+          onChangeSortType={this.onChangeSortType}
+          sortType={this.state.sorts.type}
+          onChangeFilter={this.onChangeFilter}
+          isChecked={this.state.filters.toggled}
+          onClickHiddenHogs={this.onClickHiddenHogs}
+        />
+        <HogsBrowser
+          hogs={this.state.hogs}
+        />
       </div>
     );
   }
---
+++ src/components/HogsBrowser.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import HogCard from './HogCard'
+
+class HogsBrowser extends React.Component {
+
+  renderHogCards = () => {
+    return this.props.hogs.map(hog => {
+      return <HogCard hog={hog} key={hog.id} />
+    })
+  }
+
+  render() {
+    return (
+      <div className='ui three cards'>
+        {this.renderHogCards()}
+      </div>
+    );
+  }
+}
+
+export default HogsBrowser;
